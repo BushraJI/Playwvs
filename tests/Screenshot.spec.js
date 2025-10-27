762a2f662d1d139dd798097dad89ab6c786648c4
BushraJI <135147594+BushraJI@users.noreply.github.com>
2025-10-27 12:52:33 +0500

    Update Screenshot.spec.js

--- tests/Screenshot.spec.js
+++ tests/Screenshot.spec.js
@@ -1,17 +1,21 @@
-import {test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import path from 'path';
 
-test('page screenshot',async ({ page }) => {
-    await page.goto('https://testautomationpractice.blogspot.com/')
-    await page.screenshot({path:'tests\Screenshot/'+Date.now()+'Homepage.png'})
+test('page screenshot', async ({ page }) => {
+  await page.goto('https://testautomationpractice.blogspot.com/');
+  await page.screenshot({ path: `tests/Screenshot/${Date.now()}_Homepage.png` });
 });
 
-test('Full page screenshot',async ({ page })=>  {
-    await page.goto('https://testautomationpractice.blogspot.com/')
-    await page.screenshot({path:'tests\Screenshot/'+Date.now()+'Homepage.png' ,fullPage:true})
+test('Full page screenshot', async ({ page }) => {
+  await page.goto('https://testautomationpractice.blogspot.com/');
+  await page.screenshot({
+    path: `tests/Screenshot/${Date.now()}_FullPage.png`,
+    fullPage: true
+  });
 });
 
-test('Element page screenshot',async ({ page })=>{
-   await page.goto('https://testautomationpractice.blogspot.com/')
-    await page.locator("//h2[normalize-space()='Dynamic Button']").screenshot({path:'tests\Screenshot/'+Date.now()+'Homepage.png' })
+test('Element page screenshot', async ({ page }) => {
+  await page.goto('https://testautomationpractice.blogspot.com/');
+  const element = page.locator("//h2[normalize-space()='Dynamic Button']");
+  await element.screenshot({ path: `tests/Screenshot/${Date.now()}_Element.png` });
 });
